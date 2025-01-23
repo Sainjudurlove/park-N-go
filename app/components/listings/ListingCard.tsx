97e9ccc841bd07060d2a96d0fd0f70c72ab8f9bd
Durlove <sainjudurlove@gmail.com>
2025-01-23 17:56:23 +0545

    Individual Listing View and Debugging Done

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useRouter } from "next/navigation";
-import { Listing, Reservation } from "@prisma/client";
+import { Reservation } from "@prisma/client";
 
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
@@ -13,7 +13,7 @@ import Button from "../Button";
 
 
 interface ListingCardProps {
-    data: Listing;
+    data: SafeListing;
     reservation?: Reservation;
     onAction?: (id: string) => void;
     disabled?: boolean;
